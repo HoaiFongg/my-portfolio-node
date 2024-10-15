8af6d36c5b0f0ec8f88c8e54ecd5debdc756536e
hoaifong <hoaif@hoaifong.com>
2024-10-15 19:27:31 +0700

    update

--- public/js/loadContent.js
+++ public/js/loadContent.js
@@ -5,9 +5,13 @@ async function loadHTML(elementId, file) {
         if (!response.ok) throw new Error('Network response was not ok');
         const data = await response.text();
         const element = document.getElementById(elementId);
-        console.log(`Loaded content into ${elementId}`); // Log để kiểm tra
-        element.innerHTML = data;
-        element.style.opacity = '1';  // Hiển thị phần tử sau khi tải
+        if (element) {
+            console.log(`Loaded content into ${elementId}`); // Log để kiểm tra
+            element.innerHTML = data;
+            element.style.opacity = '1';  // Hiển thị phần tử sau khi tải
+        } else {
+            console.error('Phần tử không tồn tại:', elementId);
+        }
     } catch (error) {
         console.error('Error loading HTML:', error);
     }
