c8df464a553fb5094a50d36659d734f1573d38ff
hoaifong <hoaif@hoaifong.com>
2024-10-15 19:44:42 +0700

    update

--- public/js/loadContent.js
+++ public/js/loadContent.js
@@ -1,32 +1,21 @@
 async function loadHTML(elementId, file) {
     try {
-        console.log(`Loading ${file} into ${elementId}`); // Log để kiểm tra
         const response = await fetch(file);
         if (!response.ok) throw new Error('Network response was not ok');
         const data = await response.text();
         const element = document.getElementById(elementId);
-        if (element) {
-            console.log(`Loaded content into ${elementId}`); // Log để kiểm tra
-            element.innerHTML = data;
-            element.style.opacity = '1';  // Hiển thị phần tử sau khi tải
-        } else {
-            console.error('Phần tử không tồn tại:', elementId);
-        }
+        element.innerHTML = data;
+        element.style.opacity = '1';  // Hiển thị phần tử sau khi tải
     } catch (error) {
         console.error('Error loading HTML:', error);
     }
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
+(async function () {
     await Promise.all([
         loadHTML('navbar', 'partials/navigation.ejs'),
         loadHTML('footer', 'partials/footer.ejs')
     ]);
     document.body.classList.remove('hidden'); // Hiện nội dung sau khi tải xong
-    const loadingElement = document.getElementById('loading');
-    if (loadingElement) {
-        loadingElement.classList.add('hidden'); // Ẩn loading nếu tồn tại
-    } else {
-        console.error('Phần tử loading không tồn tại');
-    }
-});
+    document.getElementById('loading').classList.add('hidden'); // Ẩn loading
+})();
