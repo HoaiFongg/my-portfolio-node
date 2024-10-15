afb87b1aaf963b3a2c3911cd79cc2f0a90945468
hoaifong <hoaif@hoaifong.com>
2024-10-15 19:24:17 +0700

    update

--- public/js/loadContent.js
+++ public/js/loadContent.js
@@ -1,9 +1,11 @@
 async function loadHTML(elementId, file) {
     try {
+        console.log(`Loading ${file} into ${elementId}`); // Log để kiểm tra
         const response = await fetch(file);
         if (!response.ok) throw new Error('Network response was not ok');
         const data = await response.text();
         const element = document.getElementById(elementId);
+        console.log(`Loaded content into ${elementId}`); // Log để kiểm tra
         element.innerHTML = data;
         element.style.opacity = '1';  // Hiển thị phần tử sau khi tải
     } catch (error) {
