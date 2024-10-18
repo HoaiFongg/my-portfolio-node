0e4a33ce797801a6352ec7ffd59f26167e9648cb
hoaifong <hoaif@hoaifong.com>
2024-10-19 01:19:35 +0700

    new

--- public/js/loadContent.js
+++ public/js/loadContent.js
@@ -13,7 +13,7 @@ async function loadHTML(elementId, file) {
 
 (async function () {
     await Promise.all([
-        loadHTML('navbar', 'partials/navbar.ejs'),
+        loadHTML('navbar', 'partials/navigation.ejs'),
         loadHTML('footer', 'partials/footer.ejs')
     ]);
     document.body.classList.remove('hidden'); // Hiện nội dung sau khi tải xong
