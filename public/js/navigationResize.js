0fc4605605bcd629173025ef8d0c41b67ab65480
hoaifong <hoaif@hoaifong.com>
2024-10-19 15:30:05 +0700

    new

--- public/js/navigationResize.js
+++ public/js/navigationResize.js
@@ -1,33 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const navbar = document.querySelector("#navbar");
+    const navbar = document.querySelector(".navbar");
 
     if (!navbar) {
-        console.error('Element with ID "header" not found.');
+        console.error('Element with class "navbar" not found.');
         return;
     }
 
-    console.log("Navbar element found:", navbar);
-
     let lastScrollY = window.scrollY;
-    let ticking = false;
 
     function handleScroll() {
         const currentScrollY = window.scrollY;
+
         if (currentScrollY > lastScrollY) {
             // Scroll down
             navbar.classList.add("small-nav");
-        } else if (currentScrollY < lastScrollY) {
+        } else {
             // Scroll up
             navbar.classList.remove("small-nav");
         }
+
         lastScrollY = currentScrollY;
-        ticking = false;
     }
 
     window.addEventListener("scroll", function () {
-        if (!ticking) {
-            window.requestAnimationFrame(handleScroll);
-            ticking = true;
-        }
+        window.requestAnimationFrame(handleScroll);
     });
 });
